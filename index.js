6fb0a5da4cd4f1444b075c50d6e0507cc8a9e884
keikun1215 <95695144+keikun1215@users.noreply.github.com>
2022-10-30 20:43:32 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,1 @@
-var http = require('http');
-var server = http.createServer(function(request, response){
-    response.writeHead(200,{'Content-Type': 'text/html; charset=utf-8'});
-    response.end('<h1><span id="hello">Hello World</span></h1>');
-})
-server.listen(8080);
-console.log(require('os').hostname(), 8080)
+
