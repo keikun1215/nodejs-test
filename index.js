47dd9d9b7a0233e44cfd2fedea8995bdffb9ccae
keikun1215 <95695144+keikun1215@users.noreply.github.com>
2022-10-30 20:50:44 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
+const { Client, Intents, Permissions } = require('discord.js');
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MEMBERS] });
 
+client.login(process.env.token)
