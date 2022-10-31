9d80a86f241ea616bac2a91ab92f57b4f2f86e62
keikun1215 <95695144+keikun1215@users.noreply.github.com>
2022-10-31 17:04:37 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
-const { Client, Intents, Permissions } = require('discord.js');
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MEMBERS] });
-require("dotenv").config()
+const { EmbedBuilder, Client, GatewayIntentBits } = require('discord.js')
+const client = new Client({
+  intents: [
+    GatewayIntentBits.DirectMessages,
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildBans,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
+  ],
+});
 
+require("dotenv").config()
 client.login(process.env.token)
