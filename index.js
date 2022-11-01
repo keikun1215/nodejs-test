39678b4dbf0c1bcfab423753abd5f8924ecf6904
keikun1215 <95695144+keikun1215@users.noreply.github.com>
2022-11-01 18:33:43 +0900

    Update index.js

--- index.js
+++ index.js
@@ -41,48 +41,72 @@ const cmds = [
         required: true
       }
     ]
+  },
+  {
+    name: 'report',
+    nameLocalizations: {
+      "ja": "メッセージを通報"
+    },
+    description: 'Report user',
+    type: 3
   }
 ]
 client.on('ready', () => {
   client.application.commands.set(cmds)
 })
 client.on('interactionCreate', async interaction => {
-  if (!interaction.isChatInputCommand()) return
-  let cmd = interaction.commandName
-  await interaction.deferReply()
-  if (cmd == "ping") {
-    await interaction.followUp({
+  if (interaction.isChatInputCommand()) {
+    let cmd = interaction.commandName
+    await interaction.deferReply()
+    if (cmd == "ping") {
+      await interaction.followUp({
+        embeds: [e({
+          title: "🏓 **Pong!**",
+          description: `ping: ${client.ws.ping}`,
+          color: 0x00FF00
+        })]
+      })
+    } else if (cmd == "help") {
+      await interaction.followUp({
+        embeds: [e({
+          title: "Help menu",
+          description: `${cmds.map(c=>`\`${c.name}\`: ${c.description}`).join("\n")}`,
+          color: 0x6395FF
+        })]
+      })
+    } else if (cmd == "ban") {
+      if(!interaction.member.permissions.has("BanMembers")) return await interaction.followUp("You not have \"BanMembers\" permission.")
+      interaction.guild.bans.create(interaction.options.get("id")).then(()=>{
+        interaction.followUp("banned")
+      }).catch(()=>{
+        interaction.followUp("failed to ban")
+      })
+    } else if (cmd == "user") {
+      const u = interaction.options.getUser("user")
+      const d = new Date(u.createdTimestamp)
+      interaction.followUp({
+        embeds: [
+          new EmbedBuilder()
+            .setThumbnail(u.displayAvatarURL() || "https://discord.com/assets/c09a43a372ba81e3018c3151d4ed4773.png")
+            .setTitle(u.tag)
+            .setDescription(`**Account created**\n${d.getFullYear()}/${d.getMonth()+1}/${d.getDate()}\n\n**ID**\n${u.id}`)
+        ]
+      })
+    }
+  } else if (interaction.isMessageContextMenuCommand()) {
+    await interaction.deferReply()
+    let message = interaction.targetMessage
+    client.channels.cache.get("1036928987370373170").send({
+      content: `Message link: ${message.url}`,
       embeds: [e({
-        title: "🏓 **Pong!**",
-        description: `ping: ${client.ws.ping}`,
-        color: 0x00FF00
+        title: `${message.athor.tag} | ${message.author.id}`,
+        description: `${message.content}`,
+        footer: {
+          text: `Report by ${interaction.user.tag}`,
+          iconURL: interaction.user.displayAvatarURL()
+        },
+        thumbnail: message.author.displayAvatarURL()
       })]
-    })
-  } else if (cmd == "help") {
-    await interaction.followUp({
-      embeds: [e({
-        title: "Help menu",
-        description: `${cmds.map(c=>`\`${c.name}\`: ${c.description}`).join("\n")}`,
-        color: 0x6395FF
-      })]
-    })
-  } else if (cmd == "ban") {
-    if(!interaction.member.permissions.has("BanMembers")) return await interaction.followUp("You not have \"BanMembers\" permission.")
-    interaction.guild.bans.create(interaction.options.get("id")).then(()=>{
-      interaction.followUp("banned")
-    }).catch(()=>{
-      interaction.followUp("failed to ban")
-    })
-  } else if (cmd == "user") {
-    const u = interaction.options.getUser("user")
-    const d = new Date(u.createdTimestamp)
-    interaction.followUp({
-      embeds: [
-        new EmbedBuilder()
-          .setThumbnail(u.displayAvatarURL() || "https://discord.com/assets/c09a43a372ba81e3018c3151d4ed4773.png")
-          .setTitle(u.tag)
-          .setDescription(`**Account created**\n${d.getFullYear()}/${d.getMonth()+1}/${d.getDate()}\n\n**ID**\n${u.id}`)
-      ]
     })
   }
 })
@@ -93,6 +117,8 @@ function e(obj){
   obj.color ? ret.setColor(obj.color) : void(0)
   obj.description ? ret.setDescription(obj.description) : void(0)
   obj.fields ? ret.addFields(obj.fields) : void(0)
+  obj.footer ? ret.addFooter(obj.footer) : void(0)
+  obj.thumbnail ? ret.addThumbnail(obj.thumbnail) : void(0)
   return ret
 }
 
