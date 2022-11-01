fa91df4e17323baddb73ab18fc50016d3ef4e52d
keikun1215 <95695144+keikun1215@users.noreply.github.com>
2022-11-01 18:50:56 +0900

    Update index.js

--- index.js
+++ index.js
@@ -116,7 +116,7 @@ function e(obj){
   obj.color ? ret.setColor(obj.color) : void(0)
   obj.description ? ret.setDescription(obj.description) : void(0)
   obj.fields ? ret.addFields(obj.fields) : void(0)
-  obj.footer ? ret.addFooter(obj.footer) : void(0)
+  obj.footer ? ret.setFooter(obj.footer) : void(0)
   obj.thumbnail ? ret.addThumbnail(obj.thumbnail) : void(0)
   return ret
 }
