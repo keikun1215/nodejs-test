7cd3960b2079adb114103165b72d33d14b199d1b
keikun1215 <95695144+keikun1215@users.noreply.github.com>
2022-11-03 12:12:00 +0900

    Create app.js

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+
+app.listen(8080, () => {
+  console.log(require("ip").address);
+});
+
+app.get('/', (req, res)=> {
+    res.json({ "pet": "dog"});
+    console.log('GETリクエストを受け取りました')
+    res.end();
+})
