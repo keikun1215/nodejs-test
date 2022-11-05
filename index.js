c3906c8cbbc59b4211702aafdd7e40ce2655da0a
keikun1215 <95695144+keikun1215@users.noreply.github.com>
2022-11-05 11:14:04 +0900

    Update index.js

--- index.js
+++ index.js
@@ -187,9 +187,11 @@ client.on('interactionCreate', async interaction => {
         .toBuffer()
       let iei = await sharp("transback.png")
         .composite([{
-          input: "38F73508-369C-462C-89AD-349807C7DB14.png"
+          input: "untrans.png"
         },{
           input: grs,
+        },{
+          input: "38F73508-369C-462C-89AD-349807C7DB14.png"
         }])
         .webp({quality: 100, lossless: true})
         .toBuffer()
