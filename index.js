0aa60e82fee531d4b30d573eda74e2fbb6e14103
keikun1215 <95695144+keikun1215@users.noreply.github.com>
2022-11-05 09:39:53 +0900

    Update index.js

--- index.js
+++ index.js
@@ -181,7 +181,7 @@ client.on('interactionCreate', async interaction => {
       let grs = await sharp(avatar)
         .grayscale()
         .webp({quality: 100})
-        .toBuffer()
+        //.toBuffer()
       interaction.followUp({files:[grs]})
     } else if (cmd == "music/play") {
       if(!interaction.member.voice.channel) return interaction.followUp("⚠️Error\nYou must join voice channel")
